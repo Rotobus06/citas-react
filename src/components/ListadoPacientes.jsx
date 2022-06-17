5240a1aa280b88a441c0bb3859599f2bd0f2b082
Rodrigo Bustos <rotobus06@gmail.com>
2022-06-17 17:38:04 -0300

    fix ListadoPacientes.jsx

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,10 +1,10 @@
 import Paciente from "./Paciente"
 
-const ListadoPacientes = ({pacientes, setPaciente}) => {
+const ListadoPacientes = ({pacientes, setPaciente, eliminarPaciente}) => {
     return (
         <div className="md:w-1/2 lg:w-3/5 mb:h-screen overflow-y-scroll">
 
-            {pacientes && pacientes.lengh ? (
+            {pacientes && pacientes.length ? (
             <>
                 <h2 className="font-black text-3xl text-center">ListadoPacientes</h2>
                 <p className="text-xl mt-5 mb-10 text-center">
